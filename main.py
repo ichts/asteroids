2b9b2a58ef7d0fa332d5919559b1b30de81b17c8
ichts <zyz10725@gmail.com>
2024-10-26 23:08:33 +0200

    creat player

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+    p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +17,9 @@ def main():
                 return
 
         screen.fill(color="black")
+        p1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
